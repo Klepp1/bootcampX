8ff5697523e4955b1040c70b2b0a314f5ef277d8
Kyler <kylerlepp8@gmail.com>
2022-06-01 12:13:17 -0700

    started the bootcampX database by creating it and adding two tables with fake data

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS students (
+  id SERIAL PRIMARY KEY NOT NULL,
+
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  phone VARCHAR(50),
+  github VARCHAR(75) NOT NULL,
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
