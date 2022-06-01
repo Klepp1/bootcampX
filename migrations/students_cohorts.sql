f7bcdf00d171f6e6a95287ea2d1abfffa7221307
Kyler <kylerlepp8@gmail.com>
2022-06-01 12:49:54 -0700

    finished the first queries section!!

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,17 +1,16 @@
 CREATE TABLE IF NOT EXISTS cohorts (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(50) NOT NULL,
+  name VARCHAR(255) NOT NULL,
   start_date DATE,
   end_date DATE
 );
 
 CREATE TABLE IF NOT EXISTS students (
   id SERIAL PRIMARY KEY NOT NULL,
-
-  name VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  phone VARCHAR(50),
-  github VARCHAR(75) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  phone VARCHAR(32),
+  github VARCHAR(255),
   start_date DATE,
   end_date DATE,
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
